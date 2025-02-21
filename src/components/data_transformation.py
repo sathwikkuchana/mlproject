74e49748c3ceea3fe7e09887b7e77c912eedbbc7
Sathwik Kuchana <kuchana.sathwik@gmail.com>
2025-02-21 16:48:10 -0500

    predection pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pk1")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 
 class DataTransformation:
