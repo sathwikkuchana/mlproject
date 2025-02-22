61be1aeb9463027a92bbfe87e4499e296d0f2eb0
Sathwik Kuchana <kuchana.sathwik@gmail.com>
2025-02-21 21:28:33 -0500

    debug

--- application.py
+++ application.py
@@ -27,14 +27,19 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            writing_score=float(request.form.get('writing_score')),
+            reading_score=float(request.form.get('reading_score'))
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
 
+                
         predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
+        try:
+             results=predict_pipeline.predict(pred_df)
+        except Exception as e:
+            application.logger.error("Prediction error: %s", e, exc_info=True)
+            return render_template('home.html', results="An error occurred during prediction")
         return render_template('home.html',results=results[0])
     
 
